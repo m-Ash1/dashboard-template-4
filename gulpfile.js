39abf60ddce962e970379a33368569f4f093ba5e
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2022-10-22 12:47:44 +0200

    upload default gulp structure

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+const prefix = require("gulp-autoprefixer");
+const sass = require("gulp-sass")(require("sass"));
+const connect = require("gulp-connect");
+const webp = require("gulp-webp");
+
+// Compile SCSS into CSS
+gulp.task("sass", function () {
+  return gulp
+    .src("src/css/**/*.scss")
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(connect.reload());
+});
+
+// copy html files
+gulp.task("html", function () {
+  return gulp.src("src/*.html").pipe(gulp.dest("dist")).pipe(connect.reload());
+});
+
+// connect server
+gulp.task("connect", function () {
+  connect.server({
+    root: "./dist/",
+    livereload: true,
+  });
+});
+
+// watch tasks
+gulp.task("watch", function () {
+  gulp.watch("src/css/**/*.scss", gulp.series("sass"));
+  gulp.watch("src/*.html", gulp.series("html"));
+});
+
+gulp.task("default", gulp.parallel("connect", "watch"));
